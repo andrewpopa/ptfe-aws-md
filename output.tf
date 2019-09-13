27b2c2386cd4ef56b069ff94e0ec3ebce482d2cd
Andrei Popa <andrei@hashicorp.com>
2019-09-13 09:06:41 +0200

    update main

--- output.tf
+++ output.tf
@@ -9,6 +9,10 @@ output "aws_ptfe_dns" {
   value = "${module.ec2.ptfe_dns}"
 }
 
+output "lb_dns" {
+  value = "${module.networking.alb_dns_name}"
+}
+
 output "ptfe_fqdn" {
   value = "${module.silent.fqdn}"
 }
